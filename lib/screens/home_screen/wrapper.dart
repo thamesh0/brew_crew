b7288a40d84252a94ff2396dc33745096b512940
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-02-28 04:54:27 +0530

    ...

---
+++ lib/screens/home_screen/wrapper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
