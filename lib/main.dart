afa49539ca234d9252f81c4e7a6f8efa46d3500b
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-09-26 11:38:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
